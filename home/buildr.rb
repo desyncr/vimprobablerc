5a017e6668a52570a811dbed15b18cf4aa5a04f1
DC* <dc@syncr.com.ar>
2013-11-30 23:30:28 -0300

    Add hompage builder

---
+++ home/buildr.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+require 'thor'
+require 'yaml'
+require 'erb'
+
+class Buildr < Thor
+  include Thor::Actions
+  class_option :home, :default => 'home.yml'
+  class_option :stylesheet, :default => 'stylesheet.css'
+  class_option :template, :default => 'template.html.erb'
+
+  desc 'build', 'Build static home page'
+  def build
+
+    home = YAML.load_file(options[:home])
+    stylesheet = File.new(options[:stylesheet], 'r').read
+
+    template = File.new(options[:template], 'r').read
+    print ERB.new(template).result(binding)
+
+  end
+
+end
+
+Buildr.start(ARGV)
